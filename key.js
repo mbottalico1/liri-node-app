79d2915ca176aac51458fcc28b8e7cc123a3c91a
Michael Bottalico <michaelbottalico@Michaels-MBP.home>
2017-06-07 12:31:34 -0400

    updated twitter

--- key.js
+++ key.js
@@ -6,3 +6,8 @@ exports.twitterKeys = {
   access_token_key: '871765107688210432-lVwXeH4PgHbGg2pOvMwdofft4tbMOEn',
   access_token_secret: '3lhMTYr7p600HU7Kc6UziQxuyCAV37Gs9W4J571XrN9tu',
 }
+
+var spotify = new Spotify({
+  id: '6d71df011b8440dd969a291ea0ba75ff',
+  secret: '65b0c99e936d4c878654b4fc6243aee8'
+});
--- liri.js
+++ liri.js
@@ -1,7 +1,31 @@
-var dataKeys = require('./keys.js');
+//required variables
+var twitterKeys = require('./key.js');
 var fs = require('fs');
 var request = require('request');
 var Twitter = require('twitter');
-var spotify = require('spotify');
-
+var Spotify = require('node-spotify-api');
+var client = new Twitter(exports.twitterKeys);
 var action = process.argv[2];
+var twitterName = process.argv[3];
+
+console.log(twitterKeys);
+
+var twitterFunction = function() {
+	var userId = twitterName || 'itsmbottalico'
+	var params = { screen_name: userId };
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+		if (!error) {
+			for (var i = 0; i < 20; i++) {
+				console.log(tweets[i].text);
+				console.log('-------');
+			}
+		}
+	});
+};
+
+	
+
+if (action === 'my-tweets') {
+	twitterFunction();
+} 
+
