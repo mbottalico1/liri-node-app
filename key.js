5ff20a0e59b9728806e80bae4d7f9a3a186a1f39
Michael Bottalico <michaelbottalico@Michaels-MacBook-Pro.local>
2017-06-08 18:30:46 -0400

    got twitter to work

--- key.js
+++ key.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: 'ijiGlXlQCKlVJvpPok82NzXdX',
-  consumer_secret: 'Wpa8pFRcCAF8sG8giMoUKl5n0qVqZD0BMsLD4hOFL24HgoBqOj',
+  consumer_key: 'lSLUrC5EB1H3qx1fEgOqzGNnS',
+  consumer_secret: 'oPqGfcCP16Ao4hZNQrpbMuEGcyqO1lh0UMtJsZAFfVk6sfPSzC',
   access_token_key: '871765107688210432-lVwXeH4PgHbGg2pOvMwdofft4tbMOEn',
   access_token_secret: '3lhMTYr7p600HU7Kc6UziQxuyCAV37Gs9W4J571XrN9tu',
 }
--- liri.js
+++ liri.js
@@ -1,15 +1,13 @@
 
 //required variables
-var twitterKeys = require('./key.js');
+var keys = require('./key.js').twitterKeys;
 var fs = require('fs');
 var request = require('request');
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
-var client = new Twitter(twitterKeys);
+var client = new Twitter(keys);
 var action = process.argv[2];
 var twitterName = process.argv[3];
-
-console.log(twitterKeys);
 
 var twitterFunction = function() {
 	var params = { screen_name: 'itsmbottalico', count: 20 };
@@ -18,6 +16,8 @@ var twitterFunction = function() {
 			for (var i = 0; i < tweets.length; i++) {
 				console.log(tweets[i].text);
 			} 
+		} else {
+			console.log(error);
 		}
 	});
 };
